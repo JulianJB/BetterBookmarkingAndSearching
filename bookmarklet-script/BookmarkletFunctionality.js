1e97cc47e3b37b02334cd0f9075c12947a167675
Julian JB <jjb21@st-andrews.ac.uk>
2020-05-25 06:58:36 +0100

    Update Bookmarklet Functionality version 1.3
    Update Bookmarklet Functionality version 1.3 changelog:
    - Added support for the Description meta tag (Page Description)

--- bookmarklet-script/BookmarkletFunctionality.js
+++ bookmarklet-script/BookmarkletFunctionality.js
@@ -7,14 +7,23 @@
  * Bookmarklet script. The script (compressed version) is in
  * another file (BookmarkletScript.js).
  *
- * Version 1.2 / Date: May 18, 2020
+ * Version 1.3 / Date: May 25, 2020
  */
 
 const url = "http://127.0.0.1:8888/bookmarking.html";
 
 var pageTitle = encodeURIComponent(document.title);
+var pageDescription = "";
 var urlEncoded = encodeURIComponent(location.href);
-var queryString = "?title=" + pageTitle + "&url=" + urlEncoded;
+
+var metaTags = document.getElementsByTagName("meta");
+for (var i in metaTags) {
+    if (typeof (metaTags[i].name) != "undefined" && metaTags[i].name.toLowerCase() == "description") {
+        pageDescription = encodeURIComponent(metaTags[i].content);
+    }
+}
+
+var queryString = "?title=" + pageTitle + "&description=" + pageDescription + "&url=" + urlEncoded;
 var redirectUrl = url + queryString;
 
 console.log("RedirectURL: ", redirectUrl);
